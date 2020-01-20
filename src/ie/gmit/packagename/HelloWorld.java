f825d50fef630f80d565de73baaff7dc43d00c7e
conatynikita <g00374513@gmit.ie>
2020-01-20 17:38:32 +0000

    Added For Loop

--- src/ie/gmit/packagename/HelloWorld.java
+++ src/ie/gmit/packagename/HelloWorld.java
@@ -3,8 +3,13 @@ package ie.gmit.packagename;
 class HelloWorld {
 
     public static void main(String[] args) {
-        // Print Hello World! to the console.
-        System.out.println("Hello World!");
+        // Print Hello Nikita! to the console.
+        
+      for (int i = 0; i < 10; i++) {
+       System.out.println("Hello Nikita!");   
+      }
+   
+        
     }
 
 }
